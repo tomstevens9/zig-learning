c8f8e95d46dc5bbcd4ac4aea01dd1decbd64f80c
Tom Stevens <tomjstevens9@gmail.com>
2025-09-05 21:45:39 +0100

    Move NumberParser into its own file

--- 04-json-parser/src/parser.zig
+++ 04-json-parser/src/parser.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const tokenizer = @import("tokenizer.zig");
 const Tokenizer = tokenizer.Tokenizer;
 const TokenizerError = tokenizer.TokenizerError;
 const string_parser = @import("string_parser.zig");
 const StringParserError = string_parser.StringParserError;
+const NumberParser = @import("number_parser.zig").NumberParser;
 
 const ParserError = error{
     UnexpectedToken,
@@ -12,7 +14,7 @@ const ParserError = error{
     MalformedUtf8,
     MalformedUnicodeEscape,
     InvalidEscapeSequence,
-} || std.mem.Allocator.Error || TokenizerError || StringParserError;
+} || Allocator.Error || TokenizerError || StringParserError;
 
 const JsonValue = union(enum) {
     object: std.StringHashMap(JsonValue),
@@ -24,7 +26,7 @@ const JsonValue = union(enum) {
 
     const Self = @This();
 
-    pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
+    pub fn deinit(self: *Self, allocator: Allocator) void {
         switch (self.*) {
             .object => |*obj| {
                 var iterator = obj.iterator();
@@ -44,117 +46,18 @@ const JsonValue = union(enum) {
     }
 };
 
-const NumberParser = struct {
-    input: []const u8,
-    pos: usize,
-
-    const Self = @This();
-
-    pub fn init(input: []const u8) NumberParser {
-        return NumberParser{ .input = input, .pos = 0 };
-    }
-
-    fn peek(self: *Self) ?u8 {
-        if (self.pos >= self.input.len) return null;
-        return self.input[self.pos];
-    }
-
-    fn consume(self: *Self) ?u8 {
-        const c = self.peek() orelse return null;
-        self.pos += 1;
-        return c;
-    }
-
-    fn parse(self: *Self) f32 {
-        const sign_multiplier: f32 = if (self.peek().? == '-') -1.0 else 1.0;
-        if (sign_multiplier == -1.0) {
-            _ = self.consume();
-        }
-        // Parse whole part
-        const whole_part = self.parseWholePart();
-        // Parse fraction part (if present)
-        const fraction_part = self.parseFractionPart();
-        // Parse exponent part (if present)
-        const exponent_part = self.parseExponentPart();
-        // Bring it all together
-        return sign_multiplier * ((whole_part + fraction_part) * std.math.pow(f32, 10, exponent_part));
-    }
-
-    fn parseWholePart(self: *Self) f32 {
-        var whole_part: f32 = 0.0;
-        // Validated number is guaranteed to have some whole part
-        const is_zero = (self.peek().?) == '0';
-        if (is_zero) {
-            _ = self.consume();
-        } else {
-            while (self.peek()) |char| {
-                if (!std.ascii.isDigit(char)) break;
-                _ = self.consume();
-                const value: f32 = @floatFromInt(char - 48);
-                whole_part *= 10.0;
-                whole_part += value;
-            }
-        }
-        return whole_part;
-    }
-
-    fn parseFractionPart(self: *Self) f32 {
-        var fraction_part: f32 = 0.0;
-        const contains_fraction_part = if (self.peek()) |char| char == '.' else false;
-        if (!contains_fraction_part) return fraction_part;
-
-        // Consume the decimal
-        _ = self.consume();
-        // Parse the digits
-        var coefficient: f32 = 0.1;
-        while (self.peek()) |char| {
-            if (!std.ascii.isDigit(char)) break;
-            _ = self.consume();
-            const value: f32 = @floatFromInt(char - 48);
-            fraction_part += (value * coefficient);
-            coefficient *= 0.1;
-        }
-        return fraction_part;
-    }
-
-    fn parseExponentPart(self: *Self) f32 {
-        var exponent_part: f32 = 0.0;
-        const contains_exponent_part = if (self.peek()) |char| (std.ascii.toLower(char) == 'e') else false;
-        if (!contains_exponent_part) return exponent_part;
-
-        // Consume the exponent symbol
-        _ = self.consume();
-        // There can be a sign after the exponent part
-        const exponent_sign_multiplier: f32 = if ((self.peek().?) == '-') -1.0 else 1.0;
-        if (self.peek()) |char| {
-            if (char == '-' or char == '+') _ = self.consume();
-        }
-        // There has to be at least one digit after exponent
-        // Parse the digits
-        while (self.peek()) |char| {
-            if (!std.ascii.isDigit(char)) break;
-            _ = self.consume();
-            const value: f32 = @floatFromInt(char - 48);
-            exponent_part *= 10.0;
-            exponent_part += value;
-        }
-        exponent_part *= exponent_sign_multiplier;
-        return exponent_part;
-    }
-};
-
 fn parseNumber(input: []const u8) f32 {
     var number_parser = NumberParser.init(input);
     return number_parser.parse();
 }
 
 const JsonParser = struct {
-    allocator: std.mem.Allocator,
+    allocator: Allocator,
     tokeniser: Tokenizer,
 
     const Self = @This();
 
-    pub fn init(allocator: std.mem.Allocator, tokeniser: Tokenizer) JsonParser {
+    pub fn init(allocator: Allocator, tokeniser: Tokenizer) JsonParser {
         return JsonParser{ .allocator = allocator, .tokeniser = tokeniser };
     }
 
@@ -242,7 +145,7 @@ const JsonParser = struct {
     }
 };
 
-pub fn parse(allocator: std.mem.Allocator, input: []const u8) ParserError!JsonValue {
+pub fn parse(allocator: Allocator, input: []const u8) ParserError!JsonValue {
     const tokeniser = Tokenizer.init(input);
     var parser = JsonParser.init(allocator, tokeniser);
     return parser.parse();
